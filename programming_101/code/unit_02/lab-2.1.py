acfb98ff021661d5ea175665da4b5c1508f90492
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-24 02:59:29 -0800

    completed lab challenge

---
+++ programming_101/code/unit_02/lab-2.1.py
@@ -0,0 +1,20 @@
+# Unit 2 Lab Extra Challenge 1
+# Math Program
+
+# create math operation
+'''
+You enter the numbers {num_1} and {num_2}
+The result is {total}
+'''
+# ask user for inputs
+print("Welcome to an mathematical addition calculator")
+print("Please enter two numbers")
+num_1 = int(input("First number: "))
+num_2 = int(input("Second number: "))
+total = num_1 + num_2
+
+# insert user inputs into MabLib
+print(f'''
+You enter the numbers {num_1} and {num_2}
+The result is {total}
+''')
