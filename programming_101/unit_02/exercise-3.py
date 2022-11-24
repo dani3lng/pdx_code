c27a2d4b97f45823355a22a3c0d55787c2742faa
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-24 01:33:23 -0800

    updated unit 1 lab link

--- programming_101/unit_02/exercise-3.py
+++
@@ -1,7 +0,0 @@
-# Unit 2 Practice
-# Exercise 3
-
-# have user enter word or phrase and print out the word or phrase with the cases of all the letters flipped
-phrase = input("Enter a word or phrase: ")
-
-print(phrase.swapcase())
