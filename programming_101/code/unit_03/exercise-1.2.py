cf279de8f7a86062e5aa38e8abae26abd2691363
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-27 22:14:58 -0800

    finished exercise 1.2

---
+++ programming_101/code/unit_03/exercise-1.2.py
@@ -0,0 +1,17 @@
+# Unit 3 Practice
+# Exercise 1
+
+# find area of triangle
+# c = 1/2 * b * h
+# h = height 
+# b = base
+
+# assign number to each
+b = 5
+h = 8
+
+# find perimeter
+a = .5 * h * b
+
+# print output
+print(f"A triangle with a base of {b} and a height of {h} has an area of {a}")
