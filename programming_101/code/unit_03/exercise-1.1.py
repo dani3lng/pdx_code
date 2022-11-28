9bb6a950780f24a4973b6c4dbc038e836df5a1f0
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-27 22:09:29 -0800

    finished exercise 1.1

---
+++ programming_101/code/unit_03/exercise-1.1.py
@@ -0,0 +1,18 @@
+# Unit 3 Practice
+# Exercise 1
+
+# create 3 variables
+# a = side x
+# b = side y
+# c = hypotenuse
+
+# assign number to each
+a = 10
+b = 12
+c = 5
+
+# find perimeter
+p = a + b + c
+
+# print output
+print(f"A triangle with sides of {a}, {b}, and {c} has a perimeter of {p}")
