a926d02ba13dc27527308948902065d8787e95b5
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-27 22:50:24 -0800

    finished exercises

---
+++ programming_101/code/unit_03/exercise-1.6.py
@@ -0,0 +1,20 @@
+# Unit 3 Practice
+# Exercise 1
+
+# find hypotenuse of a triangle
+# c^2 = a^2 + b^2
+# c = hypotenuse
+# a = height
+# b = base
+
+import math
+
+# assign number to variables
+a = 65
+b = 72
+
+# find volume
+c = math.sqrt(a**2 + b**2)
+
+# print output
+print(f"A triangle with sides of {a} and {b} has a hypotenuse of {c}")
---
+++ programming_101/code/unit_03/exercise-1.7.py
@@ -0,0 +1,12 @@
+# Unit 3 Practice
+# Exercise 1
+
+# find hypotenuse of a triangle
+# c^2 = a^2 + b^2
+# c = hypotenuse
+# a = height
+# b = base
+
+import math
+import random
+
