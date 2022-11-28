65ca56e4b8fffb8713e4ab7fd5bc904b09433d3a
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-27 22:24:25 -0800

    finished exercise 1.3

---
+++ programming_101/code/unit_03/exercise-1.3.py
@@ -0,0 +1,25 @@
+# Unit 3 Practice
+# Exercise 1
+
+# find circumference and area of a circle
+# C = 2 * pi * r or C = pi * d
+# A = pi * r^2
+
+import math
+
+# assign number to variables
+r = 5
+pi = math.pi
+
+# find circumference
+C = 2 * pi * r
+
+# find area
+A = pi * r**2
+
+# print output
+print(f'''
+Radius: {r}
+Circumference: {C}
+Area: {A}
+''')
