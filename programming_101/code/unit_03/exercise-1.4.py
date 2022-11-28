c9943f0c8ea95418a3f2ae52d690b04e170a4ef6
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-27 22:33:06 -0800

    finished exercise 1.4

---
+++ programming_101/code/unit_03/exercise-1.4.py
@@ -0,0 +1,22 @@
+# Unit 3 Practice
+# Exercise 1
+
+# find volume of a sphere
+# V = 4/3 * pi * r^3
+
+import math
+
+# assign number to variables
+r = 5
+pi = math.pi
+
+# find volume
+V = 4/3 * pi * r**3
+
+# print output
+print(f'''
+Sphere
+
+Radius: {r}
+Volume: {V}
+''')
