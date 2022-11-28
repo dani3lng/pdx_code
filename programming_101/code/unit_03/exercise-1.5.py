f27dbe6d33ef23213da4432c3bd99d07980dd096
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-27 22:39:29 -0800

    finished exercise 1.5

---
+++ programming_101/code/unit_03/exercise-1.5.py
@@ -0,0 +1,26 @@
+# Unit 3 Practice
+# Exercise 1
+
+# find area of an annulus
+# A = π (R2﹣r2)
+# R = outer radius
+# r = inner radius
+
+import math
+
+# assign number to variables
+R = 5
+r = 3 
+pi = math.pi
+
+# find volume
+A = pi * (R**2 - r**2)
+
+# print output
+print(f'''
+Annulus
+
+Outer Radius: {R}
+Inner Radius: {r}
+Area: {A}
+''')
