d79c02a43ec3b44e5065e4e3a0b53ae576d70834
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-29 02:44:52 -0800

    completed

---
+++ programming_102/code/unit_01/exercise-3.py
@@ -0,0 +1,10 @@
+# Unit 1 Practice
+# Exercise 2
+
+# find error
+# print 'The last letter of 'elephant' is: {'elephant'[8]}')    ############## SyntaxError: unmatched ')'
+                                                                ############## SyntaxError: invalid syntax ''
+                                                                ############## SyntaxError: invalid syntax ''
+                                                                ############## IndexError: string index out of range
+# corrected
+print(f'The last letter of "elephant" is: {"elephant"[7]}') 
