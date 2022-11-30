d0dfdabbaed07c0d73aaa8dc72529f5a57a6981d
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-11-29 16:04:20 -0800

    completed exercise

--- programming_102/code/unit_02/exercise-2.py
+++ programming_102/code/unit_02/exercise-2.py
@@ -3,3 +3,18 @@
 
 # create list 
 colors = []
+
+# create loop
+while True:
+    user = input("Enter a color or 'done' to quit: ")
+    # set break condition
+    if user == 'done':
+        print(f"The colors you entered were: {colors}")
+        break
+    # add user's color to list
+    if user not in colors:
+        colors.append(user)
+        print(f"'{user}' added to the colors list!")
+    # prevent adding similar colors
+    elif user in colors:
+        print(f"Oops! '{user}' is already in the colors list! ")
