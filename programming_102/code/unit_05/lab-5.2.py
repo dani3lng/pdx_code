091a4808db16a8d620f1b7ef46757255d1f0efe3
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-12-02 21:11:00 -0800

    completed 5.2

---
+++ programming_102/code/unit_05/lab-5.2.py
@@ -0,0 +1,38 @@
+# Unit 5 Lab
+# User Login
+
+# create profile dictionary
+profile = {
+    'username': 'gandalfTheGrey',
+    'password': 'noneShallPass!',
+}
+
+# create login function
+def login(username_attempt, password_attempt):
+    if username_attempt == profile['username'] and password_attempt == profile['password']:
+        return True
+    else:
+        return False
+        
+# get username and password from user
+while True:
+    usr_input = input("Enter your username: ")
+    pass_input = input("Enter your password: ")
+    usr_result = login(usr_input, pass_input)
+    if usr_result == True:
+        print('\nSuccessful login!')
+        print(f'username: {usr_input}')
+        print(f'password: {pass_input}')
+        print(f'\nWelcome, {usr_input}')
+        break
+    elif usr_result == False:
+        print('\nUnsuccessful login!')
+        print(f'username: {usr_input}')
+        print(f'password: {pass_input}')
+        print(f'\nError! Your username or password was incorrect!')
+        repeat = input('Would you like to try again (yes/no)? ')
+        if repeat == 'yes':
+            print('\n')
+            continue
+        if repeat == 'no':
+            break
