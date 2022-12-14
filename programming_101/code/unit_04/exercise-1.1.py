8e177a5b097e856496aca9aeb583b2f74ff66eae
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-12-13 17:12:13 -0800

    completed exercise 1.1

---
+++ programming_101/code/unit_04/exercise-1.1.py
@@ -0,0 +1,12 @@
+# Unit 3 Practice
+# Exercise 1.1
+
+# create list
+num_list = [3, 3, 10, 5, 4, 5, 6, 5, 7, 2]
+
+# print all number <= 5
+for x in num_list:
+    if x <= 5:
+        print(x)
+    else:
+        continue
