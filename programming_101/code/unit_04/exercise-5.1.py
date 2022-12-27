0b84f45200748311bc05b36139fda24d851fbfb2
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2022-12-27 01:24:11 -0800

    completed exercise

---
+++ programming_101/code/unit_04/exercise-5.1.py
@@ -0,0 +1,26 @@
+# Unit 4 Practice
+# Exercise 5.1
+
+import random
+
+# create lists
+number_list = []
+even_list = []
+
+# create for loop to populate list
+for x in range(1, 10):
+    number_list.append(random.randint(1,100))
+    
+# create loop to find even numbers
+even_count = 0
+for number in number_list:
+    x = number % 2
+    if x == 0:
+        even_list.append(number)
+        even_count += 1
+    else:
+        continue
+        
+# display lists
+print(f"Numbers: {number_list}")
+print(f"There are {even_count} evens: {even_list} ")
