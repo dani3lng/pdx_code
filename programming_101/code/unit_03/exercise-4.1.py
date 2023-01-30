aeb6fcba8822a50f4dc0ed4c7391d02f8032b40a
Daniel Ng <dani3l.yng@gmail.com>
2023-01-30 14:03:29 -0800

    completed exercise 4.1

---
+++ programming_101/code/unit_03/exercise-4.1.py
@@ -0,0 +1,11 @@
+# Unit 4 Practice
+# Exercise 4.1
+
+# ask for number from user
+user_number = int(input('Please enter an integer: '))
+
+# determine if number is between 10 - 100
+if user_number >= 10 and user_number <= 100:
+    print(f'{user_number} is between 10 and 100')
+else:
+    print(f'{user_number} is not between 10 and 100')
